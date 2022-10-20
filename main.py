6c13f28e108946834b22405476ef60724d862d5b
WendyLaura <116230595+WendyLaura@users.noreply.github.com>
2022-10-20 00:53:09 +0000

    Aprendi a criar variáveis e exibir na tela.

--- main.py
+++ main.py
@@ -3,7 +3,20 @@
 #print()= comando de saida
 print("Aló mundo!")
 
+#Quando quiser guardar uma String(frase)
+nome = "Wendy Laura"
+#sinal de igual = e uma atribuição
+#21 - sem aspa e indicar uma grandeza, quando queremos representar a qauntidade de alguma coisa.
+idade = 21
+#Exibir o meu nome (que esta dentro da variavel nome)
+print(nome)
+print(idade)
+
+
+
 
 '''
 Comentario em bloco
 '''
+#sinal de igual = e uma atribuição
+#21 - sem aspa e indicar uma grandeza, quando queremos representar a qauntidade de alguma coisa.
